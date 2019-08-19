641529a46416978990c8588d0caec8b3badd204f
Sergei <gannochenko.sv@gmail.com>
2019-08-19 13:59:52 +0200

    data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Lottie from 'lottie-react-web'
+import animation from './data.json'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Lottie
+            options={{
+              animationData: animation
+            }}
+        />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
